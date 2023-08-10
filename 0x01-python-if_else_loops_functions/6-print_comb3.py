d137c7a9c1ff47167ebba1ea63471bdef5bd8fa2
MarkMusili <musiliyrn@gmail.com>
2023-08-10 21:55:00 +0000

    Print diffrent combinations update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for n in range(1, 10):
-        if i == 8 and n == 9:
+        if i < n:
+            if i == 8 and n == 9:
                 print("{}{}".format(i, n), end="")
-        else:
+            else:
                 print("{}{}, ".format(i, n), end="")
+        else:
+            pass
 print()
