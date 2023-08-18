b365e3c9149156d7e19e93fee864456d29dae999
MarkMusili <musiliyrn@gmail.com>
2023-08-18 22:00:44 +0000

    Get the uncommon elements in a list

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    not_common = [[i for i in set_1 if i not in set_2] +  [n for n in set_2 if n not in set_1]]
+    i = [i for i in set_1 if i not in set_2]
+    n = [n for n in set_2 if n not in set_1]
+    not_common = [i + n]
     return not_common
