58a1c20665f97d9fd65500f250157b5c6ccc1c5d
MarkMusili <musiliyrn@gmail.com>
2023-08-29 13:51:51 +0000

    Class with optional instances

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is a module that Creates a class called square"""
+
+
+class Square:
+    """
+        This is a class representing a Square
+        It has a private attribute size
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size(int): size of the square which is first initialized to 0
+        Attributes:
+            size(int) - size of the square
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
