3e50b1996e4ac2e1b9f0203ee10423e09af735a5
MarkMusili <musiliyrn@gmail.com>
2023-08-29 13:17:45 +0000

    Create a class with an instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a module that Creates a class called square"""
+
+
+class Square:
+    """
+    Args:
+        size(int): size of the square
+    Attributes:
+        size(int) - size of the square
+    """
+    def __init__(self, size):
+            self._size = size
