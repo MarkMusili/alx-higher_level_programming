89e89fa45e02c4a929a70812dfcdab6c50728d68
MarkMusili <musiliyrn@gmail.com>
2023-08-29 16:00:55 +0000

    Print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""This is a module that Creates a class called square"""
+
+
+class Square:
+    """
+        This is a class representing a Square
+        It has a private attribute size
+    """
+    def __init__(self, size=0):
+        """
+        Args:
+            size(int): size of the square which is first initialized to 0
+        Attributes:
+            size(int) - size of the square
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+            This is a method that calulates the area of a square
+            return size * size
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Property getter for size
+        Return: int: size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        This is a private instance of self that acts as a setter
+        Return: int: value which is the new size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        This is a public instance where it prints '#' to form the square
+        Returns: '#'chars that form a square
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print('#' * self.__size)
