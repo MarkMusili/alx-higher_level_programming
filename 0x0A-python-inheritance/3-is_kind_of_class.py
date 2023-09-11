3c1b9c557559c6148dbb9f1a697705581cbccfe6
MarkMusili <musiliyrn@gmail.com>
2023-09-11 20:22:32 +0000

    Check wether inherited or not

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module checks wether an object is inherited
+from a specific class
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    Args:
+        obj: subclass to be checked
+        a_class: base class to be checked
+    Return:
+        True: if the subclass is in base class
+        False: if subclass is not it base class
+    """
+    return isinstance(obj, a_class)
