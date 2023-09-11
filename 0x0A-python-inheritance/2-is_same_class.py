1112ff46cb667383977cf9314a9f1d68ee0d5208
MarkMusili <musiliyrn@gmail.com>
2023-09-11 20:16:28 +0000

    Check wether object is in class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -14,4 +14,4 @@ def is_same_class(obj, a_class):
         True: if the object is in class
         False: if object is not it class
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
