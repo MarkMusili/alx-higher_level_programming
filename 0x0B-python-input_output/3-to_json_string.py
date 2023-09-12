0edef50c3ddf5eb0bec63b9680bfeab61b4a723c
MarkMusili <musiliyrn@gmail.com>
2023-09-12 14:23:45 +0000

    Convert from python to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+import json
+"""
+This module encodes an object to JSON format
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Args:
+        my_obj (string): object to be converted
+    Return:
+        JSON representation of the object
+    """
+    return json.dumps(my_obj)
