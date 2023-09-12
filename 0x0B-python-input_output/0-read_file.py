8a24aa045b6b59921fd5d110c9e249f048b3851b
MarkMusili <musiliyrn@gmail.com>
2023-09-12 13:21:46 +0000

    Read from file and print output to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a function that reads a text file
+and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Args:
+        filename = path of the file to be opened
+    Return:
+        Prints the contents to the stdout
+    """
+    with open(filename) as f:
+        file_contents = f.read()
+        print(file_contents)
