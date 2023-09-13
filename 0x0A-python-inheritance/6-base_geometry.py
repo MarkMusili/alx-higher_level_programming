f6bc36105e708b8799694e26ea96f83f705041bc
MarkMusili <musiliyrn@gmail.com>
2023-09-13 13:09:02 +0000

    Impliment area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a class for doing geometry
+"""
+
+
+class BaseGeometry:
+    """
+    This class has a public instnace method for calculating/
+    area
+    """
+    def area(self):
+        """
+        Raises an exception
+        """
+        raise Exception("area() is not implimented")
+
