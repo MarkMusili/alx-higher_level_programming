8b21079b92212ed37177834c1ae17e1a50aa3cbe
MarkMusili <musiliyrn@gmail.com>
2023-09-13 13:34:08 +0000

    Create a subclass

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module impliments a subclass Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This is a subclass of BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Instantation of Rectangle
+        Attributes:
+            width (int): width of rectangle
+            height (int): height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
