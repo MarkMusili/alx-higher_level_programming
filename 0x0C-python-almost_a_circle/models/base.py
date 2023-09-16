13c7da461f17693db9a16d22d018bddd8091e34f
MarkMusili <musiliyrn@gmail.com>
2023-09-16 22:31:30 +0300

    Empty library and base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a base class:
+that will the the parent class of all the other class
+"""
+
+
+class Base:
+    """
+    This class manages the id attribute \
+    for the rest of the classes
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Arg:
+            id (int): Number associated with an instance
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
