0eda1a57a821acf0bcb8294f13cf3e20a53a6cf8
MarkMusili <musiliyrn@gmail.com>
2023-09-17 16:35:42 +0300

    The square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines a square
+"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class Represents a square
+    that inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Class constructor
+        Attributes:
+            size (int): size of the square
+            x (int): spaces on the x co-ordinate
+            y (int): spaces on the y co-ordinate
+            id (int): id associated with the square
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Represent the square in user friendly manner
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
