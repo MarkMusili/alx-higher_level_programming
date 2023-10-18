4a89754fdf4535e1218a460d6f1a00c7c0acc804
MarkMusili <musiliyrn@gmail.com>
2023-10-18 15:44:28 +0000

    Inner join based on ids

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Display tv_shows based on their genre id
+SELECT tv_shows.title,tv_show_genres.genre_id 
+FROM tv_shows 
+INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.genre_id 
+ORDER BY tv_shows.title,tv_show_genres.genre_id ASC;
