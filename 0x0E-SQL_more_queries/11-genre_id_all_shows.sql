326f4433951e96f18b69d6d4c67d951f3a9e9255
MarkMusili <musiliyrn@gmail.com>
2023-10-18 20:10:36 +0000

    Left join based on ids

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Use left join to create a table
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows 
+LEFT JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
