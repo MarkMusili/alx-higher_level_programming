ed6ae0af98f64a02f798147035d3015d1db148a9
MarkMusili <musiliyrn@gmail.com>
2023-10-18 20:21:16 +0000

    Left join based on ids but only those with no genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Use left join to create a table but return only those with no genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows 
+LEFT JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
