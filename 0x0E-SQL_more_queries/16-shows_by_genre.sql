36b9b5be71c3c25e6c20d312a9d65767f9ccc201
MarkMusili <musiliyrn@gmail.com>
2023-10-19 06:47:36 +0000

    List shows with their genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,9 +3,4 @@ SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows 
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id  
-UNION  
-SELECT tv_shows.title AS title, tv_genres.name 
-FROM tv_genres 
-RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
-RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id  
 ORDER BY title,name ASC;
