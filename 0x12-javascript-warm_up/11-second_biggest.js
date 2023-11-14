4f014a611ee9dc9cbb4955cef4ad3d210dc6cc0d
MarkMusili <musiliyrn@gmail.com>
2023-11-14 07:59:08 +0000

    Syntax change

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,10 @@ const list = process.argv.slice(2).map(Number);
 if (list.length <= 1) {
   console.log(0);
 } else {
-  const max_num = Math.max(...list);
-  const index = list.indexOf(max_num);
+  const maxNum = Math.max(...list);
+  const index = list.indexOf(maxNum);
   list.splice(index, 1);
 
-  const second_max = Math.max(...list);
-  console.log(second_max);
+  const secondMax = Math.max(...list);
+  console.log(secondMax);
 }
