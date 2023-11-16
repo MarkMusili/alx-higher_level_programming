02a4b3a749255c71b52702e9233199e0540bfff4
MarkMusili <musiliyrn@gmail.com>
2023-11-16 13:27:15 +0000

    MySQLdb part

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+"""
+This module uses the Module MySQLdb to interact with a database
+"""
+
+
+def list_states(username, password, database):
+    """
+    This function lists the states from a database
+    Args:
+        mysql username: user trying to connect
+        mysql password: password for the user conncecting
+        database name: name of the database
+    """
+    # connection details
+    host = 'localhost'
+    port = 3306
+
+    # establish a concetion
+    connection = MySQLdb.connect(
+            host=host,
+            port=port,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    # Create a cursor object to execute SQL querries
+    cursor = connection.cursor()
+
+    # Query to be executed
+    query = 'SELECT * FROM states ORDER BY id'
+
+    # Execute the querry
+    cursor.execute(query)
+
+    # Fetch and print the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    username, password, database = sys.argv[1:4]
+
+    list_states(username, password, database)
