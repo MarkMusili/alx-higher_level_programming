111880da87a87bb1f8f540fe521d41b0b519b1d7
MarkMusili <musiliyrn@gmail.com>
2023-11-16 16:49:15 +0000

    MySQLdb part safe from injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+"""
+This module uses the Module MySQLdb to interact with a database
+"""
+
+
+def list_states(username, password, database, state_name):
+    """
+    This function lists the states from a database,
+    that start with 'N'
+    Args:
+        mysql username: user trying to connect
+        mysql password: password for the user conncecting
+        database name: name of the database
+        state_name: name to be searched
+    """
+    # connection details
+    host = 'localhost'
+    port = 3306
+
+    # establish a concetion
+    connection = MySQLdb.connect(
+            host=host,
+            port=port,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    # Create a cursor object to execute SQL querries
+    cursor = connection.cursor()
+
+    # Query to be executed
+    query = 'SELECT * FROM states WHERE name = %s ORDER BY states.id'
+
+    # Execute the querry
+    cursor.execute(query, (state_name,))
+
+    # Fetch and print the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    username, password, database, state_name = sys.argv[1:5]
+
+    list_states(username, password, database, state_name)
