d1c75a6224d13051e6ab710c057190077d52fe4e
MarkMusili <musiliyrn@gmail.com>
2023-11-18 13:42:36 +0000

    MySQLdb part

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """
 This module uses the Module MySQLdb to interact with a database
 """
+import sys
+import MySQLdb
+
 
 if __name__ == '__main__':
     """
