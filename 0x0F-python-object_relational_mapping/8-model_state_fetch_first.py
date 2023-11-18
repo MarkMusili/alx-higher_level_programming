502de17356700bb03aec4fa24a3ee8aeff6f4328
MarkMusili <musiliyrn@gmail.com>
2023-11-18 19:07:11 +0000

    SQLAlchemy part

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module establishes a connection to a database and
+querries from it
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """
+    This function querries from a database
+    """
+    username, password, name = sys.argv[1:]
+    # Establish a connection
+    ul = f"mysql+mysqldb://{username}:{password}@localhost:3306/hbtn_0e_6_usa"
+
+    engine = create_engine(ul, pool_pre_ping=True)
+
+    # Create a session to query
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query
+    instance = session.query(State).first()
+    if instance is None:
+        print("Nothing")
+    else:
+        print(instance.id, instance.name, sep=": ")
