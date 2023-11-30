3896a74dbb1b4c5670f5c01a8087847e0624f14f
MarkMusili <musiliyrn@gmail.com>
2023-11-30 15:34:17 +0300

    Get the length from the response header

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Get the Content-Length from the response header
+
+if [ -z "$1" ]; then
+	echo "Usage $0 <url>"
+	exit 1
+fi
+
+url=$1
+
+curl -sI "$url" | grep -i Content-Length | awk '{print $2}'
