1bc95c18063e1720feff6a1ea7e6a3a800cb6594
MarkMusili <musiliyrn@gmail.com>
2023-11-30 15:39:40 +0300

    3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/bash
 # Get the Content-Length from the response header
-
-if [ -z "$1" ]; then
-	echo "Usage $0 <url>"
-	exit 1
-fi
-
-url=$1
-
-curl -sI "$url" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
