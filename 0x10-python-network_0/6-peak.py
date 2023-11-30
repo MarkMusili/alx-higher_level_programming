ae79691203fdfc24108ec2e73d02990d400f0f22
MarkMusili <musiliyrn@gmail.com>
2023-11-30 20:54:34 +0300

    Documention fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,13 @@ Calculates the peak of an unsorted list"""
 
 
 def find_peak(list_of_integers):
-	"""This function find the peak"""
+	"""
+	This function find the peak
+	Args:
+		list_of_intergers (list): list of unsorted integers
+	Return:
+		Returns the peak number or None
+	"""
 	if list_of_integers:
 		return max(list_of_integers)
 	else:
