f604dc983dbb6fa58e0637d40b9ccc7296bc4499
MarkMusili <musiliyrn@gmail.com>
2023-12-01 15:57:58 +0300

    requiremetnt check

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,9 +4,9 @@ import sys
 import urllib.request
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    header_info = response.headers.get("X-Request-Id")
-
 if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        header_info = response.headers.get("X-Request-Id")
     print(header_info)
+
