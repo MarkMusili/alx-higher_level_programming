be98d900d47ad851c55e159fc3840272472ebee4
MarkMusili <musiliyrn@gmail.com>
2023-12-01 11:47:39 +0300

    Post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Make a POST request to the server"""
+import sys
+from urllib import request, parse
+
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = parse.urlencode({'email': email}).encode("utf-8")
+
+request_url = request.Request(url=url, data=data, method="POST")
+
+with request.urlopen(request_url) as response:
+	response_email = response.read().decode("utf-8")
+
+if __name__ == "__main__":
+	print(response_email)
+
+
