b9cec09241e3835ab4fcded1e218c6bfe9e875e5
MarkMusili <musiliyrn@gmail.com>
2023-12-03 12:01:34 +0300

    Minor issues

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,5 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-
-    print(f"Error code: {r.status_code}")
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
